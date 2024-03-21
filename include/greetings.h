b29bd09af4754c7f6f56aa86730266f4587cb0f1
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2024-03-20 21:11:05 -0300

    Add configuration files

---
+++ include/greetings.h
@@ -0,0 +1,6 @@
+#pragma once
+
+/**
+ * @brief Greet the user
+*/
+void greetings();
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <greetings.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello OSII 2024!\n");
+    greetings();
+    return 0;
+}
